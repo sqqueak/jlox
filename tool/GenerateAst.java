894748b7ee4e214a7107ae7fa255d124b15cf3f7
Emily Yao <emily2004.yao@gmail.com>
2024-03-03 14:54:06 -0600

    (5.3) working with trees

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -31,6 +31,8 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
     // AST subclasses
     for(String type : types) {
       String className = type.split(":")[0].trim();                             // String manip only runs for this specific implementation :(
@@ -38,9 +40,26 @@ public class GenerateAst {
       defineType(writer, baseName, className, fields);
     }
 
+    // The base accept() method
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
     writer.println("}");
     writer.close();
   }
+
+  private static void defineVisitor(
+    PrintWriter writer, String baseName, List<String> types) {
+      writer.println("  interface Visitor<R> {");
+
+      for(String type : types) {
+        String typeName = type.split(":")[0].trim();
+        writer.println("    R visit" + typeName + baseName + "(" + typeName 
+          + " " + baseName.toLowerCase() + ");");
+      }
+
+      writer.println("  }");
+    }
 
   private static void defineType(
     PrintWriter writer, String baseName, String className, String fieldList) {
@@ -55,6 +74,13 @@ public class GenerateAst {
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println(
+      "      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
 
     writer.println();
