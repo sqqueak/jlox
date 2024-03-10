99c3a8d037edc2e61c6d0a183a6ce12e1b7b0ccb
Emily Yao <emily2004.yao@gmail.com>
2024-03-10 18:26:40 -0500

    (7.3) runtime error

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -19,10 +19,21 @@ class Interpreter implements Expr.Visitor<Object> {
       case BANG:
         return !isTruthy(right);
       case MINUS:
+        checkNumberOperand(expr.operator, right);
         return -(double)right;
     }
 
     return null; // unreachable
+  }
+
+  private void checkNumberOperand(Token operator, Object operand) {
+    if(operand instanceof Double) return;
+    throw new RuntimeError(operator, "Operand must be a number.");
+  }
+
+  private void checkNumberOperands(Token operator, Object left, Object right) {
+    if(left instanceof Double && right instanceof Double) return;
+    throw new RuntimeError(operator, "Operands must be numbers.");
   }
 
   private boolean isTruthy(Object object) {
@@ -51,14 +62,19 @@ class Interpreter implements Expr.Visitor<Object> {
       case BANG_EQUAL: return !isEqual(left, right);
       case EQUAL_EQUAL: return isEqual(left, right);
       case GREATER:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left > (double)right;
       case GREATER_EQUAL:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left >= (double)right;
       case LESS:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left < (double)right;
       case LESS_EQUAL:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left <= (double)right;
       case MINUS:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left - (double)right;
       case PLUS:
         if(left instanceof Double && right instanceof Double) {
@@ -67,10 +83,13 @@ class Interpreter implements Expr.Visitor<Object> {
         if(left instanceof String && right instanceof String) {
           return (String)left + (String)right;
         }
-        break;
+        throw new RuntimeError(expr.operator, 
+          "Operands must be two numbers or two strings.");
       case SLASH:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left / (double)right;
       case STAR:
+        checkNumberOperands(expr.operator, left, right);
         return (double)left * (double)right;
     }
 
