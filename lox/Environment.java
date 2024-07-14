ba162730cbc67a7fa2454f6ba12fd55030560dc7
Emily Yao <emily2004.yao@gmail.com>
2024-07-14 17:20:50 -0400

    (8.4) assignment

--- lox/Environment.java
+++ lox/Environment.java
@@ -14,6 +14,19 @@ public class Environment {
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
   }
 
+  void assign(Token name, Object value) {
+    // If we're assigning a value to a variable, that means it should already
+    // exist in the environment's list of variables. If it doesn't exist then it
+    // means we can't assign a value to it without creating a new entry, which
+    // would be a definition.
+    if(values.containsKey(name.lexeme)) {
+      values.put(name.lexeme, value);
+      return;
+    }
+
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+  }
+
   void define(String name, Object value) {
     values.put(name, value);
   }
